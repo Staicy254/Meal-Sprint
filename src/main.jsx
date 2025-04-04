4c3e535da16d1ac9b4723679d7f80d93b1077352
Jean Staicy Auma Otambo <jeanstaicy21@gmail.com>
2025-04-04 18:27:59 +0300

    dist

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import StoreContextProvider from "./context/Storecontext.jsx";
+import StoreContext from "./context/StoreContext.jsx"; 
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <StoreContextProvider>
+    <StoreContext>
       <App />
-    </StoreContextProvider>
+    </StoreContext>
   </BrowserRouter>
 );
