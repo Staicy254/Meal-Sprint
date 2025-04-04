8c16f0faf8e9e2caf540565f4918a4d3975dee7d
Jean Staicy Auma Otambo <jeanstaicy21@gmail.com>
2025-04-04 16:56:37 +0300

    vite-config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Meal-Sprint/', // Ensure this matches the name of your repository
 })
